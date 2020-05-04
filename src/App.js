10fe38be1d57bf73ccd2aab47906c4ac548359c6
CamilaBernales <camilabernales09@gmail.com>
2020-05-04 13:42:56 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from '@emotion/styled'
+import imagen from './cryptomonedas.png'
+const Contenedor = styled.div
+`
+  max-width: 900px;
+  margin: 0 auto;
+  @media (min-width:992px){
+    display:grid;
+    grid-template-columns: repeat(2, 1fr); //dos columnas de una fraccion 50-50
+    column-gap: 2rem;
+  }
+`;
 
+const Imagen = styled.img`
+max-width: 100%;
+margin-top: 5rem;
+`;
+
+const Heading= styled.h1`
+  font-family: 'Bebas Neue', cursive;
+  color: #FFF;
+  text-align:left;
+  font-weight: 700;
+  font-size: 50px;
+  margin-bottom: 50px;
+  margin-top: 80px;
+
+&::after{
+  content: '';
+  width: 100px;
+  height: 6px;
+  background-color: #66A2FE;
+  display:block;
+}
+
+`;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+     <Contenedor>
+       <div>
+         <Imagen
+            src={imagen}
+            alt="imagen cripto"
+         />
+       </div>
+       <div>
+          <Heading>
+              Cotiza Criptomonedas
+          </Heading>
+       </div>
+     </Contenedor>
+ 
   );
 }
 
